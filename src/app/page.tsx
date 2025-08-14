1bed1346e6a5c7e197928eaa50638793a33fc54e
Omar-Alme <omar.almehdar17@gmail.com>
2025-08-14 02:46:52 +0200

    setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+
   return (
     <div>
-      <Button>
-        Click me
-      </Button>
+      <h1>Hello</h1>
     </div>
   );
 }
